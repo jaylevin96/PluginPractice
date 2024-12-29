fc8053a9f9c56d93a18552385f69bdce326c40fb
jayle <jayle@JAYS-PC>
2024-12-28 17:35:11 -0800

    Add project files.

---
+++ Account/Account.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xrm.Sdk;
+
+namespace Account
+{
+    public class Account : IPlugin
+    {
+        public void Execute(IServiceProvider serviceProvider)
+        {
+            IPluginExecutionContext context = (IPluginExecutionContext)serviceProvider.GetService(typeof(IPluginExecutionContext));
+            ITracingService tracingService = (ITracingService)serviceProvider.GetService(typeof(ITracingService));
+
+
+            Entity entity = (Entity)context.InputParameters["Target"];
+
+            tracingService.Trace("Step 1");
+
+            //Credit limit is 1 MIL
+            decimal maxCredit = 1000000;
+            decimal currentCredit = 0;
+            if (entity.Contains("creditlimit"))
+            {
+                currentCredit = ((Money)entity["creditlimit"]).Value;
+            }
+
+            if(currentCredit > maxCredit)
+            {
+                throw new InvalidPluginExecutionException("Credit is too high!");
+            }
+        }
+    }
+}
